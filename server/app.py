e5e45f37de35b9eb2c4b2d61264e179cccf81ceb
TrevorPhell <trevor.phell@student.moringaschool.com>
2025-05-23 08:53:54 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-# server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,7 +19,47 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
+
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+
+    if earthquake:
+        earthquake_dict = {
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }
+        return make_response(jsonify(earthquake_dict), 200)
+    else:
+        error_dict = {
+            "message": f"Earthquake {id} not found."
+        }
+        return make_response(jsonify(error_dict), 404)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    quakes_list = []
+    for earthquake in earthquakes:
+        earthquake_dict = {
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }
+        quakes_list.append(earthquake_dict)
+
+    response_dict = {
+        "count": len(quakes_list),
+        "quakes": quakes_list
+    }
+
+    return make_response(jsonify(response_dict), 200)
 
 
 if __name__ == '__main__':
